0e4af3923d824a38a8508f7a228c96eb21ab1fd1
unknown <veestereh@gmail.com>
2026-03-15 18:29:10 +0100

    SidePanel added

--- ExpensesManager/MainWindow.xaml.cs
+++ ExpensesManager/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,16 +10,15 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace ExpensesManager;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow : Window 
 {
     public MainWindow()
     {
         InitializeComponent();
     }
 }
+    
---
+++ ExpensesManager/View/UserControls/SidePanel.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ExpensesManager.View.UserControls;
+
+public partial class SidePanel : UserControl
+{
+    public SidePanel()
+    {
+        InitializeComponent();
+    }
+
+    private void BtnAddTransaction_OnClick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
